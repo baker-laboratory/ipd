9dffcb7287a74a8048e7798599420ef2aaf07410
Will Sheffler <willsheffler@gmail.com>
2024-09-02 14:39:42 -0700

    add initial ci

---
+++ ipd/tests/check_test_results.py
@@ -0,0 +1,30 @@
+import glob
+import sys
+
+def main():
+    fail = False
+
+    for f in glob.glob('pytest*.log'):
+        with open(f) as inp:
+            lines = inp.readlines()
+            fail |= 'failed' in lines[-1]
+            for line in lines:
+                fail |= 'ERROR' in line
+                fail |= 'FAILED' in line
+                fail |= 'Error while loading ' in line
+        if fail:
+            print('PYTEST FAILED:', f)
+            print(str.join('', lines))
+
+    with open('ruff.log') as inp:
+        lines = inp.readlines()
+        if 'All checks passed!' not in lines[-1]:
+            print('RUFF FAILED')
+            print(str.join('', lines))
+            fail = True
+
+    if fail:
+        sys.exit(-1)
+
+if __name__ == '__main__':
+    main()
