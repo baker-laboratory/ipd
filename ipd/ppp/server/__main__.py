c3356800d4b5f01ceb409dbf20ace8e20199c8f8
Will Sheffler <willsheffler@gmail.com>
2024-09-23 23:09:20 -0700

    WIP ppp update main again

--- ipd/ppp/server/__main__.py
+++ ipd/ppp/server/__main__.py
@@ -9,19 +9,21 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--port', type=int, default=12345)
 parser.add_argument('--dburl', type=str, default='postgresql://sheffler@localhost:5432/ppp')
 parser.add_argument('--datadir', type=str, default='/projects/ml/prettier_protein_project/server_data')
-parser.add_argument('--libdir', type=str, default=None)
+parser.add_argument('--update_libs', type=str, default=None)
 parser.add_argument('--loglevel', type=str, default='info')
 
 def update_libs(libdir):
     if not libdir: return
+    print(f'UPDATING LIBRARIES IN {libdir}')
     subprocess.check_output(f'cd {libdir}/ipd && git pull'.split(), shell=True)
     subprocess.check_output(f'cd {libdir}/willutil && git pull'.split(), shell=True)
     subprocess.check_output(f'cd {libdir}/wills_pymol_crap && git pull'.split(), shell=True)
 
 def main():
     args = parser.parse_args()
-    update_libs(args.libdir)
+    update_libs(args.update_libs)
     import ipd
+    print(f'STARTING SERVER localhost:{args.port} database: {args.dburl} datadir: {args.datadir}')
     ipd.ppp.server.run(args.port, args.dburl, args.datadir, args.loglevel)
 
 if __name__ == '__main__':
