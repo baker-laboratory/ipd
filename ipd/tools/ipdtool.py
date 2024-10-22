5dabad547b7f03ba1f3b0ad7b8d8232a6b1b1049
Will Sheffler <willsheffler@gmail.com>
2024-10-21 17:45:25 -0700

    ci fix

--- ipd/tools/ipdtool.py
+++ ipd/tools/ipdtool.py
@@ -7,6 +7,8 @@ from ipd.dev.cli.clibase import CliBase
 class IPDTool(CliBase):
     def update(self):
         repo = git.Repo(f'{ipd.projdir}/..')
+        # assert not repo.is_dirty()
+        old = repo.head.commit
         repo.remotes.origin.pull()
-        ipd.dev.bash(f'{sys.executable} -mpip install -e {ipd.projdir}/..')
-
+        if repo.head.commit != old:
+            ipd.dev.bash(f'{sys.executable} -mpip install -e {ipd.projdir}/..')
