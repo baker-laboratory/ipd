94c1024f23baf043661f17e65018f677c0b00798
Will Sheffler <willsheffler@gmail.com>
2024-11-14 00:00:47 -0800

    smarter pre-commit change detection

--- ipd/dev/code/formatting.py
+++ ipd/dev/code/formatting.py
@@ -5,6 +5,7 @@ import subprocess
 import ipd
 
 def yapf_fast(codedir: str, dryrun: bool, excludefile: str, hashfile: str, conffile: str):
+    """Run yapf on changed files in a git repo."""
     codedir, excludefile, hashfile, conffile = ipd.dev.substitute_project_vars(codedir, excludefile, hashfile, conffile)
     with ipd.dev.cd(os.path.dirname(excludefile)):
         cmd = f'find {codedir} -name *.py -exec md5sum {{}} ;'
@@ -16,10 +17,12 @@ def yapf_fast(codedir: str, dryrun: bool, excludefile: str, hashfile: str, conff
         if os.path.exists(hashfile):
             prev = set(pathlib.Path(hashfile).read_text().strip().split(os.linesep))
         diff = {x.split()[1] for x in (files - prev)} - exclude
-        yapfcmd = f'yapf -ip --style {conffile} -m {" ".join(diff)}'
         if diff:
+            yapfcmd = f'yapf -ip --style {conffile} -m {" ".join(diff)}'
             print(yapfcmd)
             if not dryrun:
+                prevhash = pathlib.Path(hashfile).read_bytes()
                 os.system(yapfcmd)
-                os.system(f'find {codedir} -name \\*.py -exec md5sum {{}} \\; > .yapf_hash')
-                exit(-1)
+                os.system(f'find {codedir} -name \\*.py -exec md5sum {{}} \\; > {hashfile}')
+                if prevhash != pathlib.Path(hashfile).read_bytes():
+                    exit(-1)
