432728b1f30c1cf15eee5914f051fcaa9bb27b7a
Will Sheffler <willsheffler@gmail.com>
2024-11-14 00:33:01 -0800

    fix install_pre_commit (#18)
    * fix install_pre_commit
    
    * smarter pre-commit change detection
    
    * smarter pre-commit change detection, try 2

--- ipd/dev/code/formatting.py
+++ ipd/dev/code/formatting.py
@@ -1,25 +1,28 @@
 import os
-import pathlib
-import subprocess
+from pathlib import Path
 
 import ipd
 
+def set_from_file(fname):
+    if os.path.exists(fname):
+        return set(Path(fname).read_text().strip().split(os.linesep))
+    return set()
+
 def yapf_fast(codedir: str, dryrun: bool, excludefile: str, hashfile: str, conffile: str):
+    """Run yapf on changed files in a git repo."""
     codedir, excludefile, hashfile, conffile = ipd.dev.substitute_project_vars(codedir, excludefile, hashfile, conffile)
     with ipd.dev.cd(os.path.dirname(excludefile)):
-        cmd = f'find {codedir} -name *.py -exec md5sum {{}} ;'
-        files = set(subprocess.check_output(cmd.split()).decode().strip().split(os.linesep))
-        exclude = set()
-        if os.path.exists(excludefile):
-            exclude = set(pathlib.Path(excludefile).read_text().strip().split())
-        prev = set()
-        if os.path.exists(hashfile):
-            prev = set(pathlib.Path(hashfile).read_text().strip().split(os.linesep))
+        cmd = fr'find {codedir} -name \*.py -exec md5sum {{}} \;'
+        prevhash = ipd.dev.run(cmd)
+        files = set(prevhash.strip().split(os.linesep))
+        exclude = set_from_file(excludefile)
+        prev = set_from_file(hashfile)
         diff = {x.split()[1] for x in (files - prev)} - exclude
-        yapfcmd = f'yapf -ip --style {conffile} -m {" ".join(diff)}'
         if diff:
+            yapfcmd = f'yapf -ip --style {conffile} -m {" ".join(diff)}'
             print(yapfcmd)
             if not dryrun:
                 os.system(yapfcmd)
-                os.system(f'find {codedir} -name \\*.py -exec md5sum {{}} \\; > .yapf_hash')
-                exit(-1)
+                os.system(f'find {codedir} -name \\*.py -exec md5sum {{}} \\; > {hashfile}')
+                if prevhash != Path(hashfile).read_text():
+                    exit(-1)
